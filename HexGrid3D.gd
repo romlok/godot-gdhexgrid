79f53db3553870405982803670491c714d9bf02a
Mel Collins <mel@raumkraut.net>
2018-06-05 21:05:16 +0200

    Add demo of 2D hex grids in 3D space

---
+++ HexGrid3D.gd
@@ -0,0 +1,25 @@
+# Script to attach to a node which represents a hex grid
+extends Spatial
+
+var HexGrid = preload("./HexGrid.gd").new()
+
+onready var plane_coords_label = get_node("Viewport/PlaneCoords")
+onready var hex_coords_label = get_node("Viewport/HexCoords")
+onready var highlight = get_node("Highlight")
+
+
+func _on_HexGrid_input_event(camera, event, click_position, click_normal, shape_idx):
+	# It's called click_position, but you don't need to click
+	var plane_coords = self.transform.affine_inverse() * click_position
+	plane_coords = Vector2(plane_coords.x, plane_coords.z)
+	# Display the coords used
+	if plane_coords_label != null:
+		plane_coords_label.text = str(plane_coords)
+	if hex_coords_label != null:
+		hex_coords_label.text = str(HexGrid.get_hex_at(plane_coords).axial_coords)
+	
+	# Snap the highlight to the nearest grid cell
+	if highlight != null:
+		var plane_pos = HexGrid.get_hex_center(HexGrid.get_hex_at(plane_coords))
+		highlight.translation.x = plane_pos.x
+		highlight.translation.z = plane_pos.y
