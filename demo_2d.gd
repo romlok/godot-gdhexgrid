192af27100abfd0e4f3946ebf7b7ac555006a2d9
Mel Collins <mel@raumkraut.net>
2018-06-11 15:03:30 +0200

    Have labels follow highligh in 2D demo

--- demo_2d.gd
+++ demo_2d.gd
@@ -3,15 +3,13 @@ extends Node2D
 
 var HexGrid = preload("./HexGrid.gd").new()
 
-onready var area_coords = get_node("AreaCoords")
-onready var hex_coords = get_node("HexCoords")
 onready var highlight = get_node("Highlight")
+onready var area_coords = get_node("Highlight/AreaCoords")
+onready var hex_coords = get_node("Highlight/HexCoords")
 
 
 func _ready():
-	if highlight != null:
-		# Work out the scale from the highlight size
-		HexGrid.hex_scale = highlight.scale
+	HexGrid.hex_scale = Vector2(50, 50)
 	
 
 func _unhandled_input(event):
